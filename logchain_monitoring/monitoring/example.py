ab77da5189f4e549c3d6e43c1f775e273d369e47
Source <ldh1428a@naver.com>
2018-05-28 14:57:34 +0900

    add_monitoring_code_version
    add_monitoring_code_version

---
+++ logchain_monitoring/monitoring/example.py
@@ -0,0 +1,7 @@
+# import sys, threading, time, queue
+# from pyqt5 import qtwidgets
+# from monitoring import form
+import time
+
+if __name__ == '__main__':
+    print(int(round(time.time()*1000.0)))

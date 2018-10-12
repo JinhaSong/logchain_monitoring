901c0b33a01b02eed7873621f9c99b466fdc09b1
DESKTOP-NTBQ7N0\Administrator <the_jn@naver.com>
2018-10-12 21:15:19 +0900

    Add transaction generating function of real data

--- demo/demo_savetx/transaction_generator.py
+++ demo/demo_savetx/transaction_generator.py
@@ -1,4 +1,7 @@
 import random
+import Adafruit_DHT
+
+
 
 def transaction_generator():
     tx= {"tx_title":None,"tx_body":None}
@@ -9,6 +12,30 @@ def transaction_generator():
     sensorValue  = {"temp":temp,"humidity": str(humidity)+"%","latitude":latitude,"Type": "led", "agent": "actuator", "duration": "7000"}
 
     tx["tx_body"] =sensorValue
+    tx["tx_title"] = "tx from gateway node"
+    return tx
+
+def transaction_generator_realData():
+    try :
+        sensor = Adafruit_DHT.DHT11
+        pin = 23
+        humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+        tx= {"tx_title":None,"tx_body":None}
+        temp = temperature
+        humidity = humidity
+        latitude = 37.5498973
+
+    except:
+        tx = {"tx_title": None, "tx_body": None}
+
+        temp = 0
+        humidity = 0
+        latitude = 0
+
+    sensorValue = {"temp": temp, "humidity": str(humidity) + "%", "latitude": latitude, "Type": "led",
+                   "agent": "actuator", "duration": "7000"}
+
+    tx["tx_body"] = sensorValue
     tx["tx_title"] = "tx from gateway node"
     return tx
 

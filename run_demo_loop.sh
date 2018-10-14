2976f9aa5dcaba688d895f7c1cd0143eebe7181c
DESKTOP-NTBQ7N0\Administrator <the_jn@naver.com>
2018-10-14 22:55:11 +0900

    Add a parameter for generating interval

--- run_demo_loop.sh
+++ run_demo_loop.sh
@@ -4,10 +4,11 @@ if [ $# -eq 0 ]
         echo "Please input request url"
     else
         url=$1
+        interval=$2
         echo "Press [CTRL+C] to stop."
         while :
         do
             python demo/demo_savetx/demo_once.py $url
-            sleep 5
+            sleep $interval
         done
 fi

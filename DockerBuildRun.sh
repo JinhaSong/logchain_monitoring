c940831bcb9adbb274dabe9b010ebcf6053dfc23
DESKTOP-NTBQ7N0\Administrator <the_jn@naver.com>
2018-10-16 23:52:15 +0900

    Modify options for runing docker

--- DockerBuildRun.sh
+++ DockerBuildRun.sh
@@ -1,3 +1,10 @@
-docker build -t logchain:monitoring .
-docker run --privileged --device /dev/gpiomem --name logchain -it logchain:monitoring /bin/bash
+docker build -t logchain:latest .
+docker run --privileged --device /dev/gpiomem --name logchain-test \
+  -p 5000:5000 \
+  -p 8000:8000 \
+  -p 8888:8888 \
+  -p 9999:9999 \
+  -p 10101:10101 \
+  -p 10654:10654 \
+  -it logchain:latest /bin/bash
 docker start logchain

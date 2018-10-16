3d9dcef032761d590735eb4981870e2e5b9a1590
DESKTOP-NTBQ7N0\Administrator <the_jn@naver.com>
2018-10-17 00:13:53 +0900

    Modify version and docker process name

--- DockerBuildRun.sh
+++ DockerBuildRun.sh
@@ -1,10 +1,10 @@
 docker build -t logchain:monitoring .
-docker run --privileged --device /dev/gpiomem --name logchain-test \
+docker run --privileged --device /dev/gpiomem --name logchain_monitoring \
   -p 5000:5000 \
   -p 8000:8000 \
   -p 8888:8888 \
   -p 9999:9999 \
   -p 10101:10101 \
   -p 10654:10654 \
-  -it logchain:latest /bin/bash
+  -it logchain:monitoring /bin/bash
 docker start logchain

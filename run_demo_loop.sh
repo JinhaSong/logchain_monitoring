17960f3a32da6f13fd8f547a37511a29821bcf27
DESKTOP-NTBQ7N0\Administrator <the_jn@naver.com>
2018-10-17 15:00:14 +0900

    Modify URL to send transaction

--- run_demo_loop.sh
+++ run_demo_loop.sh
@@ -8,7 +8,7 @@ if [ $# -eq 0 ]
         echo "Press [CTRL+C] to stop."
         while :
         do
-            python demo/demo_savetx/demo_once.py $url
+            python demo/demo_savetx/demo_once.py "http://"$url":5000/tx/save"
             sleep $interval
         done
 fi
